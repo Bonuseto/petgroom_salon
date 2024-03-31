e739c66b0e9f02463ead4ba9fefb7425771f6cdb
Yevgeniy Fomin <bonuseto@gmail.com>
2024-03-31 14:38:47 +0200

    fix direction button colouring

--- src/components/LocationMap/LocationMap.tsx
+++ src/components/LocationMap/LocationMap.tsx
@@ -9,7 +9,7 @@ import { useTranslation } from 'react-i18next';
 import classes from './LocationMap.module.css';
 
 const salonPosition = { lat: 51.083752458581884, lng: 17.038425091441333 };
-const cityCenterPosition = { lat: 51.09587635516876, lng: 17.03272634796446 };//51.09587635516876, 17.03272634796446
+const cityCenterPosition = { lat: 51.09587635516876, lng: 17.03272634796446 };
 
 const openGoogleMaps = () => {
   const url = 'https://www.google.com/maps/place/BEST+GROOM+STUDIO+Nina+Vorona/@51.0836649,17.0377478,19z/data=!3m1!4b1!4m6!3m5!1s0x470fc2f7c44f30b1:0xfa473c5008e1f423!8m2!3d51.0836649!4d17.0383929!16s%2Fg%2F11f01qx71q?hl=ru-RU&entry=ttu';
@@ -37,15 +37,17 @@ const LocationMap = () => {
             </Map>
           </APIProvider>
         </Box>
-        <Button
-          className={classes.setDirection}
-          variant="contained"
-          startIcon={<DirectionIcon />}
-          onClick={openGoogleMaps}
-          sx={{ m: 1 }}
-        >
-          al. Armii Krajowej 6c/4, 50-541
-        </Button>
+        <div>
+          <Button
+            className={classes.setDirection}
+            variant="contained"
+            startIcon={<DirectionIcon />}
+            onClick={openGoogleMaps}
+            sx={{ m: 1 }}
+          >
+            al. Armii Krajowej 6c/4, 50-541
+          </Button>
+        </div>
       </Box>
     </Container>
   );
