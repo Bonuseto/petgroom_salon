165fd158f23ed6ecdc2d8ee606469431640d3d9d
Yevgeniy Fomin <bonuseto@gmail.com>
2024-04-01 19:50:38 +0200

    Drawer refactored

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,24 @@ import React from 'react';
 import './App.css';
 import Menu from './components/Menu/Menu';
 import LocationMap from './components/LocationMap/LocationMap';
-import './i18n'; // Import i18n configuration
-import { useTranslation } from 'react-i18next';
 
 function App (): JSX.Element {
-  const { t } = useTranslation(); // Initialize useTranslation hook
   return (
     <div className="App">
       <Menu />
-      <h1>{t('pageTitle')}</h1> {/* Translate the title using t() function */}
       <header className="App-header">
-        <img id="tomikandnina" className="n9wygc" alt="Obraz nagłówka witryny" src="https://lh3.googleusercontent.com/p/AF1QipNaOBh6YWi2Q4bOV3HF31lkZbuHEhnNBvPUlJYu=w1080-h608-p-no" data-iml="695420.2999999998"></img>
+        <img id="aboutUsId" className="n9wygc" alt="About us" src="https://lh3.googleusercontent.com/p/AF1QipNaOBh6YWi2Q4bOV3HF31lkZbuHEhnNBvPUlJYu=w1080-h608-p-no" data-iml="695420.2999999998"></img>
       </header>
       <header className="App-header">
-        <img id="dingo" className="n9wygc" alt="Obraz nagłówka witryny" src="https://lh3.googleusercontent.com/p/AF1QipONkhTkm5jt1x5BdXxiFO6w6cLuBdhjkhYF1RSY=w768-h768-n-o-v1"></img>
+        <img id="servicesId" className="n9wygc" alt="Services" src="https://www.caraphil.org/mainsite/wp-content/uploads/2019/09/BASIC-GROOMING-RATES-300x242.png"></img>
       </header>
       <header className="App-header">
-        <img id="mochalka" className="n9wygc" alt="Obraz nagłówka witryny" src="https://lh3.googleusercontent.com/p/AF1QipPOlDgU758bj2XnQjewZmUth71CV2xLzaUwZHqw=w768-h768-n-o-v1" ></img>
+        <img id="pricesId" className="n9wygc" alt="Prices" src="https://edit.org/img/blog/7pr-price-list-templates-edit-editable-personalize-free-easy-fast-print-bandamp-w-black-white-pricing.webp" ></img>
       </header>
-      <div id="how-to-find">
+      <header className="App-header">
+        <img id="reviewsId" className="n9wygc" alt="Reviews" src="https://smashballoon.com/wp-content/uploads/2020/09/google-reviews-feed-example.jpg" ></img>
+      </header>
+      <div id="howToFindId">
         <LocationMap />
       </div>
       <header className="App-header">
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,8 +1,7 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
+import React, { type ReactElement } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import MuiAppBar, { type AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import CssBaseline from '@mui/material/CssBaseline';
 import List from '@mui/material/List';
@@ -10,84 +9,39 @@ import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
 import LanguageSwitch from '../LanguageSwitch/LanguageSwitch';
 import i18n from '../../i18n';
 import { useState } from 'react';
+import { Button } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+
+interface Props {
+  window?: () => Window
+}
 
 const drawerWidth = 240;
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
-  open?: boolean
-}>(({ theme, open }) => ({
-  flexGrow: 1,
-  padding: theme.spacing(3),
-  transition: theme.transitions.create('margin', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen
-  }),
-  marginRight: -drawerWidth,
-  ...((open ?? false) && {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginRight: 0
-  }),
-  position: 'relative'
-}));
+const salonName = 'BEST DOG STUDIO';
+// const LocationMap: React.FC = (): ReactElement => {
+// : React.FC = (): ReactElement
+const DrawerAppBar: React.FC = (props: Props): ReactElement => {
+  const { t } = useTranslation();
 
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean
-}
+  const navItems = [
+    { text: `${t('aboutUs')}`, id: 'aboutUsId' },
+    { text: `${t('services')}`, id: 'servicesId' },
+    { text: `${t('prices')}`, id: 'pricesId' },
+    { text: `${t('reviews')}`, id: 'reviewsId' },
+    { text: `${t('map')}`, id: 'howToFindId' }];
 
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open'
-})<AppBarProps>(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen
-  }),
-  ...((open ?? false) && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginRight: drawerWidth
-  })
-}));
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  padding: theme.spacing(0, 1),
-  ...theme.mixins.toolbar,
-  justifyContent: 'flex-start'
-}));
-
-export default function PersistentDrawerRight (): JSX.Element {
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = (): void => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = (): void => {
-    setOpen(false);
-  };
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [selectedLanguage, setSelectedLanguage] = useState<string | null>('en');
-
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  const handleLanguageSelect = (language: string) => {
-    setSelectedLanguage(language);
-    void i18n.changeLanguage(language);
+  const handleDrawerToggle = (): void => {
+    setMobileOpen((prevState) => !prevState);
   };
 
   const scrollTo = (id: string): void => {
@@ -97,74 +51,102 @@ export default function PersistentDrawerRight (): JSX.Element {
     }
   };
 
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        { salonName }
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem
+            key={item.text}
+            onClick={() => { scrollTo(item.id); }}
+            disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [selectedLanguage, setSelectedLanguage] = useState<string | null>('en');
+
+  const handleLanguageSelect = (language: string): void => {
+    setSelectedLanguage(language);
+    void i18n.changeLanguage(language);
+  };
+
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{
+      display: 'flex',
+      justifyContent: 'flex-end', // Align items to the right
+      p: 1, // Padding around the container for some space
+      mb: '49px'
+    }}>
       <CssBaseline />
-      <AppBar position="fixed" open={open} sx={{ backgroundColor: '#0ABAB5' }}>
-        <Toolbar >
+      <AppBar component="nav" sx={{ backgroundColor: '#0ABAB5' }}>
+        <Toolbar>
           <LanguageSwitch
             icon={require('./globe.png')}
             onSelectLanguage={handleLanguageSelect}
           />
-          <Typography variant="h6" noWrap sx={{ flexGrow: 1 }} component="div">
-            BEST GROOM STUDIO
-          </Typography>
           <IconButton
             color="inherit"
             aria-label="open drawer"
-            edge="end"
-            onClick={handleDrawerOpen}
-            sx={{ ...(open && { display: 'none' }) }}
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' }, marginLeft: 'auto' }}
           >
             <MenuIcon />
           </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            { salonName }
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button
+                key={item.text}
+                sx={{ color: '#fff' }}
+                onClick={() => { scrollTo(item.id); }}>
+                {item.text}
+              </Button>
+            ))}
+          </Box>
         </Toolbar>
       </AppBar>
-      <Main open={open}
-        sx={{ p: 0, height: 56 }}>
-        <DrawerHeader />
-        <Typography id="pictures" paragraph>
-          {/* Pictures content */}
-        </Typography>
-        <Typography paragraph>
-          {/* More pictures content */}
-        </Typography>
-      </Main>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth
-          }
-        }}
-        variant="persistent"
-        anchor="right"
-        open={open}
-      >
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          {[
-            { text: 'Tomik and Nina', id: 'tomikandnina' },
-            { text: 'Brown doggis', id: 'dingo' },
-            { text: 'How to find us', id: 'how-to-find' },
-            { text: 'Noseboy', id: 'nose' }
-          ].map(({ text, id }) => (
-            <ListItem
-              button
-              key={text}
-              onClick={() => { scrollTo(id); }} // Scroll to the section when clicked
-            >
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          anchor="right"
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      {/* <Box id='MAINBOX' component="main" sx={{ p: 3 }}>
+        <Toolbar />
+      </Box> */}
     </Box>
   );
-}
+};
+
+export default DrawerAppBar;
